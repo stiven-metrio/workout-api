64a91df78b059d07ebe47844155162852f00c5e2
Jhonatan Sepulveda <jhonatanmetrio@gmail.com>
2024-03-27 11:24:35 -0500

    feat(workout-definition): add javadocs for bootloader
    #1

--- bootloader/src/main/java/com/workout/config/CommandBusConfig.java
+++ bootloader/src/main/java/com/workout/config/CommandBusConfig.java
@@ -8,9 +8,20 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Configuration class for setting up the command bus.
+ * <p>
+ * This class is annotated with @Configuration, indicating that it provides bean definitions for the application context.
+ */
 @Configuration
 public class CommandBusConfig {
 
+	/**
+	 * Configures and provides a command bus bean.
+	 *
+	 * @param applicationContext The application context used for bean resolution.
+	 * @return The configured command bus instance.
+	 */
 	@Bean
 	public CommandBus commandBus(ApplicationContext applicationContext) {
 		return new LoggingCommandBus(new SpringCommandBus(new Registry(applicationContext)));
